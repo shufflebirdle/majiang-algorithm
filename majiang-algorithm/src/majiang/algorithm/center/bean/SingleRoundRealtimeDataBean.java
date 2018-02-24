9af1c34acc3aa36152c8540c4f88855cbe3b21e4
zhoupeng <zhoupeng@eptok.com>
2018-02-24 18:05:18 +0800

    新增单局实时数据对象

---
+++ majiang-algorithm/src/majiang/algorithm/center/bean/SingleRoundRealtimeDataBean.java
@@ -0,0 +1,16 @@
+package majiang.algorithm.center.bean;
+
+/**
+ * 单局游戏实时数据缓存对象
+ * @author yspc18
+ *
+ */
+public class SingleRoundRealtimeDataBean {
+	/**
+	 * 单局编号
+	 */
+	private String _id;
+	
+	private String 
+
+}
